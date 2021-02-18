b8cde74312ffd2c0690de4221848af2f01abb7a0
YukiYamada <cat.of.attic@gmail.com>
2021-02-18 15:20:14 +0900

    add token

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,6 +3,12 @@ module SessionsHelper
   def log_in(user)
     session[:user_id] = user.id
   end
+  # ユーザーのセッションを永続的にする
+  def remember(user)
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
   #  現在ログインユーザーがいる場合、ユーザーを返す
   def current_user
     if session[:user_id]
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_13_081228) do
+ActiveRecord::Schema.define(version: 2021_02_17_012709) do
 
   create_table "mysteries", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "mystery_img"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2021_02_13_081228) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
+    t.string "remember_digest"
   end
 
 end
