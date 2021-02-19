41d2bf8dbc7236182ca2c30dfdcb824a6d480c08
YukiYamada <cat.of.attic@gmail.com>
2021-02-19 18:51:38 +0900

    mysteries index change image

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -9,18 +9,32 @@ module SessionsHelper
     cookies.permanent.signed[:user_id] = user.id
     cookies.permanent[:remember_token] = user.remember_token
   end
-  #  現在ログインユーザーがいる場合、ユーザーを返す
+  # 記憶トークンcookieに対応するユーザーを返す
   def current_user
-    if session[:user_id]
-      @current_user ||= User.find_by(id: session[:user_id])
+    if (user_id = session[:user_id])
+      @current_user ||= User.find_by(id: user_id)
+    elsif (user_id = cookies.signed[:user_id])
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
     end
   end
   #　ユーザーがログイン状態か否かの確認
   def logged_in?
     !current_user.nil?
   end
+
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
+
   # 現在のユーザーをログアウトする
   def log_out
+    forget(current_user)
     session.delete(:user_id)
     @current_user = nil
   end
